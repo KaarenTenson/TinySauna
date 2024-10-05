8838f1ca0a1ea4c7a105882d93a1875bd912bb07
Kaaren Tenson <tensonkaaren@gmail.com>
2024-10-05 14:32:51 +0300

    menu

---
+++ settings.gd
@@ -0,0 +1,22 @@
+extends Panel
+@onready var vtext: Label = $Control/vtext
+@onready var h_slider: HSlider = $Control/HSlider
+@onready var audio_stream_player: AudioStreamPlayer = $"../AudioStreamPlayer"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	h_slider.value=50;
+	audio_stream_player.volume_db=linear_to_db(h_slider.value)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	audio_stream_player.volume_db=linear_to_db(value)
+	vtext.text=str(int(value))+"%";
