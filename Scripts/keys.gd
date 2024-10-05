779c9dc50499591d4d9af439cc484336eea78e75
Kaaren Tenson <tensonkaaren@gmail.com>
2024-10-05 21:00:17 +0300

    fail effekt

--- Scripts/keys.gd
+++ Scripts/keys.gd
@@ -10,13 +10,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if(timer<0):
 		Globals.Dec_Multiplier()
-		queue_free()
+		Globals.Recive_Damage()
+		anim.play("fail")
+		anim.animation_finished.connect(havita)
+		timer=10
+		
 	else:
 		timer-=delta
 
 func Destroy_Self():
-	anim.animation_finished.connect(havita)
 	anim.play("key_press")
+	anim.animation_finished.connect(havita)
+	
 
 func havita(a):
 	queue_free()
