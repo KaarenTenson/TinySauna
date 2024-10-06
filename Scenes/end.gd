5920563e302c38c2f26c333be79c4e02469d4430
Gregor Artur <144439475+greeeegartur@users.noreply.github.com>
2024-10-06 07:12:17 +0300

    outro + helid

--- Scenes/end.gd
+++ Scenes/end.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AudioStreamPlayer2D.play()
 	pass # Replace with function body.
 
 
@@ -11,5 +11,6 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/hub.tscn")
+func _on_button_end_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	pass # Replace with function body.
