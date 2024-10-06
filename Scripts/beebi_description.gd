0f7ce64d51cd06a71d827dd7a89484a883a5c6d4
rauladamson <raul.adamson@ut.ee>
2024-10-06 04:43:06 +0300

    Added baby descriptions

--- Scripts/beebi_description.gd
+++ Scripts/beebi_description.gd
@@ -1,10 +1,10 @@
 extends Control
-
+var beebi_descriptions = {"jijitsuBeebi": "With powerful legs of steel, this warrior is known as an energetic fighter. Able to fracture the bones of his opponents with a graze, this combatant is banned in 18 countries and numerous states.", "KungFuBeebi": "A prodigy in his craft - this kung fu user is a ferocious combatant, who shows mercy to few. Featured in many high-quality films, he is an idol for many young martial artists.", "TaekWonDooBeebi": "Originating from the deep mountains in the Himalayas, he is considered a master at his martial art. Being a pacifist, he is known to be merciful. Many consider him an “Avatar of Peace”.", "KarateBeebi": "This fearsome combatant uses his strong durability to his advantage - because of this, he is feared across the globe. It is said he hailed from a dojo of snakes."}
+var beebi_coolnames = {"jijitsuBeebi": "res://Assets/CoolNames/jiu-jitsu-baby.png", "KungFuBeebi": "res://Assets/CoolNames/kung-fu-baby.png", "TaekWonDooBeebi": "res://Assets/CoolNames/taekwondo-baby.png", "KarateBeebi": "res://Assets/CoolNames/karate-baby.png"}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,9 +12,11 @@ func _process(delta: float) -> void:
 
 
 func _on_vasak_pressed() -> void:
-	pass
+	$BeebiName.texture = load(beebi_coolnames[Globals.ChosenBeebi])
+	$BeebiDescription.text = beebi_descriptions[Globals.ChosenBeebi]
 
 
 
 func _on_parem_pressed() -> void:
-	pass # Replace with function body.
+	$BeebiName.texture = beebi_coolnames[Globals.ChosenBeebi]
+	$BeebiDescription.text = beebi_descriptions[Globals.ChosenBeebi]
