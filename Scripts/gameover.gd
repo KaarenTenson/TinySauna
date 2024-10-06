e12cfbb399d58c861122ab7f89f27c1251c366ff
Gregor Artur <144439475+greeeegartur@users.noreply.github.com>
2024-10-06 05:12:33 +0300

    game over update

--- Scripts/gameover.gd
+++ Scripts/gameover.gd
@@ -11,8 +11,4 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
-
-
-func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/hub.tscn")
